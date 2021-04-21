259d0feb5d5aade329bbc44dbea81bde2e9f6ced
Lex <50260829+4gname@users.noreply.github.com>
2021-04-22 00:57:21 +0700

    Create impact_lsc.lua

---
+++ component/impact_lsc.lua
@@ -0,0 +1,20 @@
+local impact_lsc = {}
+
+-- Get all info. Stored, Capacity, Input, Output
+function impact_lsc.getStoredLSC()
+    -- return array
+end
+
+-- Get capacity percent
+function impact_lsc.getChargePercent()
+    -- return number
+end
+
+-- Get selected stored percent, stored < per = -1, stored == per = 0, stored > per = 1
+function impact_lsc.getSelectChargePercent(per)
+    -- per:number
+
+    -- return number
+end
+
+return impact_lsc
